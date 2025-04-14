505fa3715a05c63b54ab78bd889b1e1e566e8879
vesimies <113595365+vesimies@users.noreply.github.com>
2025-04-14 11:42:19 +0300

    Made the code more readable

--- queries.sql
+++ queries.sql
@@ -1,15 +1,19 @@
 -- Number of the distinct campaigns
 SELECT COUNT(DISTINCT utm_campaign)
 FROM page_visits;
+
 -- Number of distinct sources
 SELECT COUNT(DISTINCT utm_source)
 FROM page_visits;
+
 -- How they are related
 SELECT DISTINCT utm_campaign, utm_source
 FROM page_visits;
+
 --Pages that are on the CoolTShirts website
 SELECT DISTINCT(page_name)
 FROM page_visits;
+
 --How many first touches for each campaign?
 WITH first_touch AS (
     SELECT user_id,
@@ -31,6 +35,7 @@ COUNT(*) AS first_touch_count
 FROM ft_att
 GROUP BY 1
 ORDER BY 3 DESC;
+
 --How many last touches for each campaign?
 WITH last_touch AS (
     SELECT user_id,
@@ -52,13 +57,16 @@ COUNT(*) AS last_touch_count
 FROM lt_att
 GROUP BY 1
 ORDER BY 3 DESC;
+
 -- How many visitors purchase?
 SELECT COUNT(*) AS all_who_purchase
 FROM page_visits
 WHERE page_name = '4 - purchase';
+
 --How many visitors?
 SELECT COUNT(DISTINCT user_id)
 FROM page_visits;
+
 --How many purchases from each campaign?
 WITH last_touch AS (
     SELECT user_id,
